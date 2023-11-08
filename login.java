a3ec5544e1602bc230cff5c2c0acb3e28aad9041
Ubuntu <ubuntu@ip-172-31-11-195.ap-south-1.compute.internal>
2023-11-08 12:13:20 +0000

    added Login.java

---
+++ login.java
@@ -0,0 +1,29 @@
+// Java code to illustrate method 
+// overloading in println() 
+
+import java.io.*; 
+
+class PrintLN { 
+	public static void main(String[] args) 
+	{ 
+
+		// Declaring different datatypes 
+		int num = 10; 
+		char ch = 'G'; 
+		String str = "GeeksforGeeks"; 
+		double d = 10.2; 
+		float f = 13.5f; 
+		boolean bool = true; 
+
+		// Various overloads of println() method 
+		System.out.println(); 
+		System.out.println(num); 
+		System.out.println(ch); 
+		System.out.println(str); 
+		System.out.println(d); 
+		System.out.println(f); 
+		System.out.println(bool); 
+		System.out.println("Hello"); 
+	} 
+} 
+
